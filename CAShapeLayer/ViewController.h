21984542d3022dcc56ad08d54d1b7a1e8c9a60e3
Artem Abramov <ar.abramov@gmail.com>
2015-04-20 16:56:59 +0300

    Initial Commit

---
+++ CAShapeLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAShapeLayer
+//
+//  Created by Artem Abramov on 20/04/15.
+//  Copyright (c) 2015 E-Legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CAShapeLayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CAShapeLayer
+//
+//  Created by Artem Abramov on 20/04/15.
+//  Copyright (c) 2015 E-Legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
